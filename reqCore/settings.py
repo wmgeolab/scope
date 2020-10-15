4c2c1f21d92228b9faefd525f778a772e63c9eb7
DanRunfola <dan@danrunfola.com>
2020-10-15 14:13:40 -0400

    Static file location for Azure

--- reqCore/settings.py
+++ reqCore/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = '/site/wwwroot/static/'
 STATIC_URL = '/static/'
 
 
