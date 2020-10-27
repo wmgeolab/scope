21285822aaf179868446cc490282d10eceb64752
lclime812 <lclime812@gmail.com>
2020-10-26 22:28:49 -0400

    Scraper added to gdelt_source_urls
    Had trouble checking if MentionIdentifier is in the database but other than that shoud be good

--- resources/gdelt_source_urls/main.py
+++ resources/gdelt_source_urls/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import sqlite3
+import urllib.request
+import zipfile
 
 def iter_gdelt_sources(file_url):
 
@@ -7,19 +10,49 @@ def iter_gdelt_sources(file_url):
     # store only the language code
     mentions['MentionDocTranslationInfo'] = mentions['MentionDocTranslationInfo'].str[6:9]
 
-
     # loop through rows and yield the columns:
     # file_url
     # MentionType
     # MentionSourceName
     # MentionIdentifier
     # MentionDocTranslationInfo (only the language code)
-    ment_dict = dict.fromkeys(['file_url', 'MentionType', 'MentionSourceName', 'MentionIdentifier', 'MentionDocTranslationInfo'])
+    ment_dict = dict.fromkeys(['url', 'MentionType', 'MentionSourceName', 'MentionIdentifier', 'MentionDocTranslationInfo'])
+
+    ment_dict['url'] = file_url
 
     for column in ['MentionType', 'MentionSourceName', 'MentionIdentifier', 'MentionDocTranslationInfo']:
         data = mentions[column]
         ment_dict[column] = data
 
-    ment_dict['file_url'] = file_url
-
     yield ment_dict
+
+def scrape_gdelt_sources():
+
+    # connect to scope database
+    conn = pymysql.connect(host="mysql.scopedata.org", user="scopesql",
+                         password="fY7Ukl52UI", db="scopesourcedata")
+
+    curs = conn.cursor()
+
+
+    # read master file list
+    masterfileurl = "http://data.gdeltproject.org/gdeltv2/masterfilelist.txt"
+
+
+    masterfilelist = urllib.request.urlopen(masterfileurl)
+
+    for line in masterfilelist:
+        if 'mentions' in line:
+            # extract url from masterfilelist
+            index = line.find('http:')
+            url = line[index:]
+
+            # loop through csv with iter_gdelt_sources
+            zf = zipfile.ZipFile(url)
+            iter_gdelt_sources(zf.open(url[:-4]))
+            # slice is to drop the .zip extension from the url
+
+            # "If row key 'MentionIdentifier' does not already exist in the database:
+                # Add to database"
+
+            # ^^not entirely sure how to do this
