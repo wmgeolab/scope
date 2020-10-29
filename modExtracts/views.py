fa5901c2795c90b2017362eff196c08ded8b8b89
rrrrli <rli10@email.wm.edu>
2020-10-29 14:51:56 -0400

    Worked with source_list (issue 28)

--- modExtracts/views.py
+++ modExtracts/views.py
@@ -7,3 +7,7 @@ def home(request, pk = pk):
     except:
         continue = None
     return render(request, 'templates/extracts/home.html', {'continue':continue,})
+
+def source_list():
+    sources = Source.objects.filter(current_user = None)
+    return render(request, 'templates/extracts/source_list.html', {'sources':sources,})
