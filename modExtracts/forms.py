1dfabc2ea848cd725cd770a690d4c662486c319c
Karim Bahgat <karim.bahgat.norway@gmail.com>
2020-11-27 16:58:44 +0100

    Extracts source extraction page rendering (#31)

--- modExtracts/forms.py
+++ modExtracts/forms.py
@@ -1,7 +1,11 @@
 
-from django.forms import ModelForm
+from django.forms import ModelForm, modelformset_factory
 
+from .models import Extract
 
+ExtractFormSet = modelformset_factory(Extract,
+                                      fields=('text'),
+                                      extra=1)
 
 
 
