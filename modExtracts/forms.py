28b60a5bda63c7e17d7c9c35cc9dded26b817d50
Karim Bahgat <karim.bahgat.norway@gmail.com>
2020-11-27 17:02:42 +0100

    Fix minor syntax errors

--- modExtracts/forms.py
+++ modExtracts/forms.py
@@ -4,7 +4,7 @@ from django.forms import ModelForm, modelformset_factory
 from .models import Extract
 
 ExtractFormSet = modelformset_factory(Extract,
-                                      fields=('text'),
+                                      fields=('text',),
                                       extra=1)
 
 
