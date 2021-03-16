c3c8890e6ae29b5fdbd852a13272ba92d13714ac
Karim Bahgat <karim.bahgat.norway@gmail.com>
2021-03-17 00:12:22 +0100

    Handle and display common errors that arise from "illegal" operations due to old pages or links

--- apps/extracting_m/views.py
+++ apps/extracting_m/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from sourcing_m.models import Source
 
@@ -14,30 +15,54 @@ def source_list(request):
     # get all sources
     sources = Source.objects.filter(current_status='SRCM') #filter(current_user = None)
 
-    # check if user already has checked out a source
-    try:
-        cont = Source.objects.get(current_user=request.user)
-        print(cont)
-    except:
-        cont = None
+    # check if user is busy
+    error = user_is_busy(request.user, None)
+    if error:
+        continue_source = error['busy_with']
+    else:
+        continue_source = None
 
-    return render(request, 'templates/extracting_m/source_list.html', {'sources':sources, 'cont':cont})
+    return render(request, 'templates/extracting_m/source_list.html', {'sources':sources, 'continue_source':continue_source})
 
 def source_checkout(request, pk):
-    source = Source.objects.get(pk = pk)
+    source = Source.objects.get(pk=pk)
+    
+    # redirect if illegal source checkout
+    error = check_illegal_source_checkout(request.user, source)
+    if error:
+        print('illegal action, redirect')
+        messages.error(request, error)
+        return redirect('source_list')
+    
     source.current_user = request.user
     source.save()
     return redirect('source_extraction', pk)
 
 def source_release(request, pk):
     source = Source.objects.get(pk = pk)
+    
+    # redirect if illegal source modification
+    error = check_illegal_source_modification(request.user, source)
+    if error:
+        print('illegal action, redirect')
+        messages.error(request, error)
+        return redirect('source_list')
+    
     source.current_user = None
     source.save()
     return redirect('source_list')
 
 def source_extraction(request, pk):
+    source = Source.objects.get(pk=pk)
+
+    # redirect if illegal source modification
+    error = check_illegal_source_modification(request.user, source)
+    if error:
+        print('illegal action, redirect')
+        messages.error(request, error)
+        return redirect('source_list')
+    
     if request.method == 'GET':
-        source = Source.objects.get(pk=pk)
         extracts = source.extracts.all()
         formset = ExtractFormSet(queryset=Extract.objects.filter(pk__in=extracts),
                                  initial=[{'source':pk}]
@@ -47,7 +72,6 @@ def source_extraction(request, pk):
                       )
 
     elif request.method == 'POST':
-        source = Source.objects.get(pk=pk)
         extracts = source.extracts.all()
 
         # get data
@@ -108,5 +132,77 @@ def source_extraction(request, pk):
             print('save')
             # only save, stay on the same page (reload the extraction page)
             return redirect('source_extraction', pk)
+
+
+
+# Put utility functions here
+
+def user_is_busy(user, source):
+    # check if the user already has checked out another source (ie different from a reference source pk)
+    print('checking if user is busy')
+    try:
+        user_source = Source.objects.get(current_user=user)
+    except Source.DoesNotExist:
+        user_source = None
+    print(user_source, source)
+    if user_source and user_source != source:
+        errormsg = {'type':'user_busy', 'busy_with':user_source}
+        print(errormsg)
+        return errormsg
+
+def source_is_busy(user, source):
+    print('checking if source is busy', user, source, source.current_user)
+    # check if someone else is currently working on this source
+    if source.current_user and source.current_user != user:
+        errormsg = {'type':'source_busy', 'current_user':source.current_user}
+        print(errormsg)
+        return errormsg
+
+def source_needs_checkout(user, source):
+    print('checking if source needs checkout', user, source, source.current_user)
+    # check if the source needs to be checked out
+    if source.current_user is None:
+        errormsg = {'type':'source_needs_checkout'}
+        print(errormsg)
+        return errormsg
+
+def check_illegal_source_modification(user, source):
+    # do a number of checks to see if user is "illegally" trying to modify a source
+    # returning the first encountered error
+    
+    # check
+    err = user_is_busy(user, source)
+    if err:
+        msg = "You tried to modify a source different than the one you already have checked out."
+        return msg
+
+    # check
+    err = source_is_busy(user, source)
+    if err:
+        msg = "You tried to modify a source that is already checked out by {}. Please checkout a different source.".format(err['current_user'])
+        return msg
+
+    # check
+    err = source_needs_checkout(user, source)
+    if err:
+        msg = "You tried to modify a source that isn't checked out. Please checkout the source first."
+        return msg
+
+def check_illegal_source_checkout(user, source):
+    # do a number of checks to see if user is "illegally" trying to checkout a source
+    # returning the first encountered error
+    
+    # check
+    err = user_is_busy(user, source)
+    if err:
+        msg = "You tried to checkout a source different than the one you already have checked out."
+        return msg
+
+    # check
+    err = source_is_busy(user, source)
+    if err:
+        msg = "You tried to checkout a source that is already checked out by {}. Please checkout a different source.".format(err['current_user'])
+        return msg
+
 
 
