14be48f6b08726528ba36a0353f7d16c54b57e22
Karim Bahgat <karim.bahgat.norway@gmail.com>
2021-03-17 11:43:16 +0100

    Handle and notify of common errors in parsing module

--- apps/parsing_m/views.py
+++ apps/parsing_m/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.apps import apps
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 import os
 
@@ -24,33 +26,54 @@ def extract_list(request):
     #how do I get only the extracts which have been submitted, since we don't have a "current_status" field in the extract model
     extracts = Extract.objects.filter(current_status="EXTQ")
 
-    # check if user already has checked out a source
-    try:
-        cont = Extract.objects.get(current_user=request.user)
-        print(cont)
-    except:
-        cont = None
+    # check if user is logged in or busy
+    if request.user.is_anonymous:
+        messages.warning(request, 'You need to login before performing a task.')
+        continue_extract = None
+    else:
+        continue_extract = user_is_busy(request.user, Extract)
 
-    return render(request, 'templates/parsing_m/extract_list.html', {'extracts':extracts, 'cont':cont})
+    return render(request, 'templates/parsing_m/extract_list.html', {'extracts':extracts, 'continue_extract':continue_extract})
 
 # extract_checkout (formerly activity_checkout)
+@login_required
 def extract_checkout(request, pk):
     extract = Extract.objects.get(pk=pk)
+
+    # redirect if illegal extract checkout
+    error = check_illegal_object_checkout(request, extract)
+    if error:
+        return redirect('extract_list')
+    
     extract.current_user = request.user
     extract.save()
     return redirect('extract_parse', pk)
 
 # extract_release (formerly activity_release)
+@login_required
 def extract_release(request, pk):
     extract = Extract.objects.get(pk=pk)
+
+    # redirect if illegal extract modification
+    error = check_illegal_object_modification(request, extract)
+    if error:
+        return redirect('extract_list')
+    
     extract.current_user = None
     extract.save()
     return redirect('extract_list')
 
 # extract_parse (formerly activity_edit)
+@login_required
 def extract_parse(request, pk):
+    extract = Extract.objects.get(pk=pk)
+
+    # redirect if illegal extract modification
+    error = check_illegal_object_modification(request, extract)
+    if error:
+        return redirect('extract_list')
+    
     if request.method == 'GET':
-        extract = Extract.objects.get(pk=pk)
         #check if the activity has been created before, or if this first time
         try:
             activity = Activity.objects.get(extract=extract)
@@ -66,7 +89,6 @@ def extract_parse(request, pk):
         return render(request, 'templates/parsing_m/extract_parse.html', context)
 
     elif request.method == 'POST':
-        extract = Extract.objects.get(pk=pk)
         #if you want to edit an existing entry, you have to give it that instance
         try:
             activity = Activity.objects.get(extract=extract)
@@ -106,69 +128,96 @@ def extract_parse(request, pk):
 
 # Put utility functions here
 
-def user_is_busy(user, extract):
-    # check if the user already has checked out another extract (ie different from a reference extract)
-    print('checking if user is busy')
+def user_is_busy(user, obj=None, model=None):
+    '''Check if the user has already checked out any object (if given model class)
+    or another object different from a reference object (if given model instance),
+    and if so return that object. 
+    Can be any model class or model instance with a 'current_user' field.
+    '''
+    #print('checking if user is busy', user, obj)
+    
+    # check if given instance or model, and get the model class
+    if obj:
+        # get model class from instance
+        model = obj._meta.model
+    elif model:
+        # model class already given
+        pass
+    else:
+        raise Exception('Either obj or model args must be given')
+
+    print(model)
+    # get object if user is busy
     try:
-        user_extract = Extract.objects.get(current_user=user)
-    except Extract.DoesNotExist:
-        user_extract = None
-    print(user_extract, extract)
-    if user_extract and user_extract != extract:
-        errormsg = {'type':'user_busy', 'busy_with':user_extract}
-        print(errormsg)
-        return errormsg
+        user_busy_with = model.objects.get(current_user=user)
+    except model.DoesNotExist:
+        user_busy_with = None
 
-def extract_is_busy(user, extract):
-    print('checking if extract is busy', user, extract, extract.current_user)
-    # check if someone else is currently working on this source
-    if extract.current_user and extract.current_user != user:
-        errormsg = {'type':'extract_busy', 'current_user':extract.current_user}
-        print(errormsg)
-        return errormsg
+    # return
+    if user_busy_with and user_busy_with != obj:
+        return user_busy_with
 
-def extract_needs_checkout(user, extract):
-    print('checking if extract needs checkout', user, extract, extract.current_user)
-    # check if the source needs to be checked out
-    if extract.current_user is None:
-        errormsg = {'type':'extract_needs_checkout'}
-        print(errormsg)
-        return errormsg
+def object_is_busy(user, obj):
+    '''Check if someone else is currently working on this object, and if so return the current user.
+    Can be any model instance with a 'current_user' attr.
+    '''
+    #print('checking if object is busy', user, obj, obj.current_user)
+    if obj.current_user and obj.current_user != user:
+        return obj.current_user
 
-def check_illegal_extract_modification(user, extract):
-    # do a number of checks to see if user is "illegally" trying to modify an extract
-    # returning the first encountered error
+def object_needs_checkout(user, obj):
+    '''Check if the object needs to be checked out, and if so return True.
+    Can be any model instance with a 'current_user' attr.
+    '''
+    #print('checking if object needs checkout', user, obj, obj.current_user)
+    if obj.current_user is None:
+        return True
+
+def check_illegal_object_modification(request, obj):
+    '''Do a number of checks to see if request.user is "illegally" trying to modify an object,
+    returning the first encountered error, and add it to message notification. 
+    '''
+    model_name = obj._meta.verbose_name
+    user = request.user
     
     # check
-    err = user_is_busy(user, extract)
+    err = user_is_busy(user, obj)
     if err:
-        msg = "You tried to modify an extract different than the one you already have checked out."
+        msg = "You tried to modify a {object_type} different than the one you already have checked out.".format(model_name)
+        messages.error(request, msg)
         return msg
 
     # check
-    err = extract_is_busy(user, extract)
+    err = object_is_busy(user, obj)
     if err:
-        msg = "You tried to modify an extract that is already checked out by {}. Please checkout a different extract.".format(err['current_user'])
+        msg = "You tried to modify a {object_type} that is already checked out by {current_user}. Please checkout a different {object_type}.".format(object_type=model_name, current_user=err)
+        messages.error(request, msg)
         return msg
 
     # check
-    err = extract_needs_checkout(user, extract)
+    err = object_needs_checkout(user, obj)
     if err:
-        msg = "You tried to modify an extract that isn't checked out. Please checkout the extract first."
+        msg = "You tried to modify a {object_type} that isn't checked out. Please checkout the {object_type} first.".format(object_type=model_name)
+        messages.error(request, msg)
         return msg
 
-def check_illegal_extract_checkout(user, extract):
-    # do a number of checks to see if user is "illegally" trying to checkout an extract
-    # returning the first encountered error
+def check_illegal_object_checkout(request, obj):
+    '''Do a number of checks to see if request.user is "illegally" trying to checkout an object,
+    returning the first encountered error, and add it to message notification. 
+    '''
+    model_name = obj._meta.verbose_name
+    user = request.user
     
     # check
-    err = user_is_busy(user, extract)
+    err = user_is_busy(user, obj)
     if err:
-        msg = "You tried to checkout an extract different than the one you already have checked out."
+        msg = "You tried to checkout a {object_type} different than the one you already have checked out.".format(object_type=model_name)
+        messages.error(request, msg)
         return msg
 
     # check
-    err = source_is_busy(user, extract)
+    err = object_is_busy(user, obj)
     if err:
-        msg = "You tried to checkout an extract that is already checked out by {}. Please checkout a different extract.".format(err['current_user'])
+        msg = "You tried to checkout a {object_type} that is already checked out by {current_user}. Please checkout a different {object_type}.".format(object_type=model_name, current_user=err)
+        messages.error(request, msg)
         return msg
