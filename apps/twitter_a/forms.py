a2cdc8d758dd31f95a2cdfb7b85c1b8e7cd53775
moses alexander <esotericgravity@gmail.com>
2021-05-03 14:31:25 -0400

    Update forms.py

--- apps/twitter_a/forms.py
+++ apps/twitter_a/forms.py
@@ -1,15 +1,19 @@
 import datetime
 
 from django import forms
+from .models import TwitterSearch
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
-class TwitterSearchForm(forms.Form):
+class TwitterSearchForm(forms.ModelForm):
     primary_keywords = forms.CharField(help_text="\nEnter primary keywords\n\n")
     secondary_keywords = forms.CharField(help_text="\nEnter secondary keywords\n\n")
     tertiary_keywords = forms.CharField(required=False, help_text="\nEnter tertiary keywords\n\n")
     start_date = forms.DateTimeField(help_text="\nPlease enter a date (later than March 2006)\n\n")
     end_date = forms.DateTimeField(help_text="\nPlease enter a date (up to or earlier than the current date)\n\n")
+    class Meta:
+        model = TwitterSearch
+        fields = '__all__'
 
     def clean_primary_kw(self):
         data = self.cleaned_data['primary_kw']
