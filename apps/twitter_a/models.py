3156d68780b1d7265da2ef0287c0126d3714aba4
moses alexander <esotericgravity@gmail.com>
2021-05-06 17:55:39 -0400

    Update models.py

--- apps/twitter_a/models.py
+++ apps/twitter_a/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
-
-class TwitterSearch(models.Model):
-    primary_keywords = models.CharField(max_length=100)
-    secondary_keywords = models.CharField(max_length=100)
-    tertiary_keywords = models.CharField(max_length=100)
-    start_date = models.DateTimeField(max_length=100)
-    end_date = models.DateTimeField(max_length=100)
-
 class TwitterSource(models.Model):
-    source_id = models.TextField(primary_key=True, max_length=50, blank=True, unique = True)
-    source_url = models.URLField(blank=True, unique = True)
-    source_text = models.TextField(blank=True, null=True)
-    source_date = models.TextField(blank=True, null=True)
+    source_id = models.CharField(max_length=100)
+    source_url = models.CharField(max_length=100)
+    source_text = models.CharField(max_length=100)
+    source_date = models.CharField(max_length=100)
     class Meta:
         managed = False
         db_table = 'twitter_a_twittersource'
