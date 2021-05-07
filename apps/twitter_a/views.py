4cbd1b5f6d19f2c36b7262cc2cfe01507cc33dba
moses alexander <esotericgravity@gmail.com>
2021-05-06 22:43:54 -0400

    Update views.py

--- apps/twitter_a/views.py
+++ apps/twitter_a/views.py
@@ -3,6 +3,7 @@ from django.apps import apps
 from django.template import Context
 from django.core.files.storage import FileSystemStorage
 
+import datetime
 import os
 from .forms import TwitterSearchForm
 from .models import *
@@ -18,7 +19,6 @@ def twitter_import(request):
         form = TwitterSearchForm(request.POST)
         vals = []
         if form.is_valid():
-            form.save(commit=False)
             pkw = []
             skw = []
             tkw = []
@@ -27,8 +27,9 @@ def twitter_import(request):
             for i in form.cleaned_data['tertiary_keywords'].split(','):  tkw.append(i.strip())
             tweets = twitter_search(form.cleaned_data['start_date'].strftime('%m/%d/%Y'), form.cleaned_data['end_date'].strftime('%m/%d/%Y'), pkw, skw, tkw)
             for x in tweets:
-                TwitterSource.objects.create(source_id=x.source_id, source_text=x.source_text, source_date=x.source_created_at, source_url=x.source_url)
+                vals.append(TwitterSource.objects.create(source_search_id=x.source_id+datetime.datetime.now().strftime("%m/%d/%Y-%H:%M:%S"), source_id=x.source_id, source_text=x.source_text, source_date=x.source_created_at, source_url=x.source_url))
             vals = TwitterSource.objects.all()
+            print(vals)
             return render(request, "templates/twitter_a/results.html", {'vals': vals})
     else:  form = TwitterSearchForm()
     return render(request, 'templates/twitter_a/search.html', {'form': form})
@@ -36,4 +37,3 @@ def twitter_import(request):
 def twitter_results(request):
     if request.method == 'POST':
         return render(request, 'templates/twitter_a/results.html')
-
