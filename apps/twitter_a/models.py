075fde07b80c3b7c69e0009f734ab0875b1eb01a
moses alexander <esotericgravity@gmail.com>
2021-05-06 20:52:11 -0400

    Update models.py

--- apps/twitter_a/models.py
+++ apps/twitter_a/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 class TwitterSource(models.Model):
+    source_search_id = models.CharField(primary_key=True, max_length=100, default='.')
     source_id = models.CharField(max_length=100)
     source_url = models.CharField(max_length=100)
     source_text = models.CharField(max_length=100)
     source_date = models.CharField(max_length=100)
-    class Meta:
-        managed = False
-        db_table = 'twitter_a_twittersource'
