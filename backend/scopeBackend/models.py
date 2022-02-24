a14835a876b8df829e47f4370206ba8f68a151e8
MichaelFoster <michaelrfoster0@gmail.com>
2022-02-24 14:54:57 -0500

    Fixed Many-to-one relationship between run and result models

--- backend/scopeBackend/models.py
+++ backend/scopeBackend/models.py
@@ -46,54 +46,17 @@ class Source(models.Model):
     def __str__(self):
         return self.url
 
-class Result(models.Model):
+class Run(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
-    src = models.ForeignKey(Source, on_delete=models.CASCADE)
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Result(models.Model):
+    run = models.ForeignKey(Run, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
-
-class Run(models.Model):
-    query = models.ForeignKey(Query, on_delete=models.CASCADE)
-    result = models.ForeignKey(Result, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-"""
-class ScopeBackend(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-"""
-
-"""
-class KeyWord(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
-    user_id = models.PositiveIntegerField(null=True, blank=True)
-    name = models.CharField(max_length=120)
-    #start_datetime = models.DateTimeField(default=datetime.now, blank=True)
-
-    def __str__(self):
-        return self.title
-
-
-class Query(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
-    keyWords = models.CharField(max_length=120)
-    start_datetime = models.DateTimeField()
-    start_date = models.DateField(null=True, blank=True)
-    start_time = models.TimeField(null=True, blank=True)
-    end_datetime = models.DateTimeField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
-    end_time = models.TimeField(null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-"""
