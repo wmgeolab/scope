67c051094d92a7e6444cf359d68a2cd2d708079b
Isabella Wu <itwu@wm.edu>
2022-04-08 16:56:22 -0400

    resizing window no longer hides nav bar

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -455,12 +455,12 @@ const Dashboard = () => {
       </div>
 
       {/* <!-- Scripts --> */}
-      <script src="assets/js/jquery.min.js"></script>
+      {/* <script src="assets/js/jquery.min.js"></script>
       <script src="assets/js/jquery.dropotron.min.js"></script>
       <script src="assets/js/browser.min.js"></script>
       <script src="assets/js/breakpoints.min.js"></script>
       <script src="assets/js/util.js"></script>
-      <script src="assets/js/main.js"></script>
+      <script src="assets/js/main.js"></script> */}
     </div>
   );
 };
