b04930bf7aab412b51c757e7520c08ff9984d8fb
jasonlin45 <jasonlin45@outlook.com>
2022-04-07 20:43:18 -0400

    Allow post for query💪

--- backend/scopeBackend/serializers.py
+++ backend/scopeBackend/serializers.py
@@ -1,16 +1,48 @@
 from rest_framework import serializers
-from .models import User, Query, Source, Result
+from .models import User, Query, Source, Result, KeyWord
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first', 'last')
 
+# class KeywordSerializer(serializers.StringRelatedField):
+#     def to_internal_value(self, data):
+#         return KeyWord(word=data)
+class KeywordSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        return str(instance)
+    
+    def to_internal_value(self, data):
+        return {'word': data}
+
+    class Meta:
+        model = KeyWord
+        fields = ('word',)
+
 class QuerySerializer(serializers.ModelSerializer):
     # keywords foreign key -> query primary key
-    keywords = serializers.StringRelatedField(many=True)
+    # keywords = serializers.StringRelatedField(many=True)
+    keywords = KeywordSerializer(many=True, read_only=False)
+    # keywords = serializers.ListField(
+    #     # child=KeywordSerializer(many=True)
+    # )
     # user foreign key -> username field
     user = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    def create(self, validated_data):
+        # print(validated_data)
+        kws = validated_data['keywords']
+        print(kws)
+        q = Query.objects.create(
+            user=validated_data['user'],
+            name=validated_data['name'],
+            description=validated_data['description']
+        )
+        for kw in kws:
+            KeyWord.objects.create(query=q, word=kw['word'])
+        return q
+
     class Meta:
         model = Query
         fields = ('id', 'name', 'description', 'user', 'keywords')
--- backend/scopeBackend/views.py
+++ backend/scopeBackend/views.py
@@ -8,6 +8,7 @@ from .models import User, Query
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework import status
 
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
@@ -27,11 +28,23 @@ class QueryView(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = QuerySerializer
     queryset = Query.objects.all()
+
+
+    # def create(self, request):
+    #     request.data['user'] = str(request.user)
+    #     return super().create(request)
     def create(self, request):
-        print(self.request.headers)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=self.request.user)
+        # self.perform_create(serializer=serializer)
+        headers = self.get_success_headers(serializer.data)
         print(self.request.user.id)
         # q = self.get_object()
-        return Response({'status': 'post received'})
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        # return Response({'status': 'post received'})
+
+
 
 class GithubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
