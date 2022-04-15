a27ca9f57ec5dd52e2735d3b4a443508cc3278da
Isabella Wu <itwu@wm.edu>
2022-04-15 16:32:23 -0400

    copied queries page code into results page

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,12 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-  
 const Results = () => {
-  return (
-    <div>
-      <h2>Results</h2>
+  const [queries, setQueries] = useState([]);
+
+  const handleSubmit = async () => {
+    let response = await fetch("http://127.0.0.1:8000/api/queries/", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + localStorage.getItem("user"),
+      },
+    });
+    let q = await response.json();
+
+    console.log(q);
+    setQueries(q);
+    return q;
+    //   .then((res) => res.json())
+    //   .then((response) => {
+    //     console.log("Success: ", JSON.stringify(response));
+    //   })
+    //   .catch((error) => console.error("Error: ", error));
+  };
+
+  useEffect(() => {
+    handleSubmit();
+  }, []); //listening on an empty array
+
+  if(localStorage.getItem("user") === null) {    // fix?
+    return(
+      <div>
+      <h2>lol</h2>
     </div>
-  );
-};
+    );
+  }
+  else {
+    return (
+      <div>
+        <title>SCOPE</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
+        />
+        <link rel="stylesheet" href="assets/css/table.css" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <div id="page-wrapper">
+          {/* <!-- Header --> */}
+          <section id="header" className="wrapper">
+            {/* <!-- Logo --> */}
+            <div id="logo">
+              <h1>
+                <a>SCOPE</a>
+              </h1>
+            </div>
   
+            {/* <!-- Nav --> */}
+            <nav id="nav">
+              <ul>
+                <li>
+                  <a href="/">Dashboard</a>
+                </li>
+                <li className="current">
+                  <a href="/queries">Queries</a>
+                </li>
+                <li>
+                  <a href="/results">Results</a>
+                </li>
+                {/* <li><a href='/login'>Login</a></li> */}
+              </ul>
+            </nav>
+          </section>
+  
+          {/* <!-- Main --> */}
+          <section id="main" className="wrapper style2">
+            <div className="title">Results</div>
+            <table className="content-table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Keywords</th>
+                  <th>User</th>
+                </tr>
+              </thead>
+              <tbody>
+                {queries.map((query, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{query.id}</td>
+                      <td>{query.name}</td>
+                      <td>{query.keywords.join(", ")}</td>
+                      <td>{query.user}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+
+            <div className="container">
+              {/* <!-- Features --> */}
+              <section id="features">
+                <ul className="actions special">
+                  <li>
+                    <a href="/create-query" className="button style1 large">
+                      Create New Query
+                    </a>
+                  </li>
+                </ul>
+                {/* <button onClick={handleSubmit}>Look at Queries</button> */}
+              </section>
+            </div>
+          </section>
+        </div>
+  
+        {/* <!-- Scripts --> */}
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.dropotron.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+      </div>
+    );
+
+  }
+};
+
 export default Results;
