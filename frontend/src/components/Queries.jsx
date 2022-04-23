428edef14b89789458f626bd25aa283245606ec6
Isabella Wu <itwu@wm.edu>
2022-04-23 17:56:09 -0400

    added functionality for query search bar

--- frontend/src/components/Queries.jsx
+++ frontend/src/components/Queries.jsx
@@ -36,6 +36,30 @@ const Queries = () => {
     //  <a href="/dashboard">Dashboard</a>;
   };
 
+  function search() {
+    // Declare variables
+    var input, filter, table, tr, td1, td2, i, txtValue1, txtValue2;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("query-table");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td1 = tr[i].getElementsByTagName("td")[1];
+      td2 = tr[i].getElementsByTagName("td")[2];
+      if (td1 && td2) {
+        txtValue1 = td1.textContent || td1.innerText;
+        txtValue2 = td2.textContent || td2.innerText;
+        if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
   if (localStorage.getItem("user") === null) {
     // fix?
     return (
@@ -94,7 +118,7 @@ const Queries = () => {
             <input
               type="text"
               id="search"
-              onkeyup="myFunction()"
+              onKeyUp={search}
               placeholder="Search queries.."
             />
 
