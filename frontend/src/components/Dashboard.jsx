29dd44a0c90b0ef4a6990dcc28ac21b4ef56890b
Isabella Wu <itwu@wm.edu>
2022-09-28 22:08:01 -0400

    minor changes to frontend design

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -54,7 +54,7 @@ const Dashboard = () => {
         <div>
           {login ? (
             <button onClick={handleLogout}>Logout</button>
-          ) : (
+            ) : (
             <LoginGithub //github gives back code give to backend, backend has client id and client secret (never transmit the secret)
               className="button style1 large"
               clientId="75729dd8f6e08419c896"
@@ -66,6 +66,7 @@ const Dashboard = () => {
 
         {/* <!-- Header --> */}
         <section id="header" className="wrapper">
+
           {/* <!-- Logo --> */}
           <div id="logo">
             <h1>
@@ -92,13 +93,15 @@ const Dashboard = () => {
             </ul>
           </nav>
         </section>
+
+        
         {/* <!-- Intro --> */}
         <section id="intro" className="wrapper style1">
-          <div className="title">The Introduction</div>
+          <div className="title">About</div>
           <div className="container">
-            <p className="style1">
+            {/* <p className="style1">
               So in case you were wondering what this is all about ...
-            </p>
+            </p> */}
             <p className="style2">
               Escape Velocity is a free responsive
               <br className="mobile-hide" />
@@ -116,135 +119,9 @@ const Dashboard = () => {
               , so use it for any of your personal or commercial projects
               &ndash; just be sure to credit us!
             </p>
-            <ul className="actions">
-              <li>
-                <a href="#" className="button style3 large">
-                  Proceed
-                </a>
-              </li>
-            </ul>
           </div>
         </section>
-        <section id="footer" className="wrapper">
-          <div className="title">The Rest Of It</div>
-          <div className="container">
-            <header className="style1">
-              <h2>Ipsum sapien elementum portitor?</h2>
-              <p>
-                Sed turpis tortor, tincidunt sed ornare in metus porttitor
-                mollis nunc in aliquet.
-                <br />
-                Nam pharetra laoreet imperdiet volutpat etiam feugiat.
-              </p>
-            </header>
-            <div className="row">
-              <div className="col-6 col-12-medium">
-                {/* <!-- Contact Form --> */}
-                <section>
-                  <form method="post" action="#">
-                    <div className="row gtr-50">
-                      <div className="col-6 col-12-small">
-                        <input
-                          type="text"
-                          name="name"
-                          id="contact-name"
-                          placeholder="Name"
-                        />
-                      </div>
-                      <div className="col-6 col-12-small">
-                        <input
-                          type="text"
-                          name="email"
-                          id="contact-email"
-                          placeholder="Email"
-                        />
-                      </div>
-                      <div className="col-12">
-                        <textarea
-                          name="message"
-                          id="contact-message"
-                          placeholder="Message"
-                          rows="4"
-                        ></textarea>
-                      </div>
-                      <div className="col-12">
-                        <ul className="actions">
-                          <li>
-                            <input
-                              type="submit"
-                              className="style1"
-                              value="Send"
-                            />
-                          </li>
-                          <li>
-                            <input
-                              type="reset"
-                              className="style2"
-                              value="Reset"
-                            />
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </form>
-                </section>
-              </div>
-              <div className="col-6 col-12-medium">
-                {/* <!-- Contact --> */}
-                <section className="feature-list small">
-                  <div className="row">
-                    <div className="col-6 col-12-small">
-                      <section>
-                        <h3 className="icon solid fa-home">Mailing Address</h3>
-                        <p>
-                          Untitled Corp
-                          <br />
-                          1234 Somewhere Rd
-                          <br />
-                          Nashville, TN 00000
-                        </p>
-                      </section>
-                    </div>
-                    <div className="col-6 col-12-small">
-                      <section>
-                        <h3 className="icon solid fa-comment">Social</h3>
-                        <p>
-                          <a href="#">@untitled-corp</a>
-                          <br />
-                          <a href="#">linkedin.com/untitled</a>
-                          <br />
-                          <a href="#">facebook.com/untitled</a>
-                        </p>
-                      </section>
-                    </div>
-                    <div className="col-6 col-12-small">
-                      <section>
-                        <h3 className="icon solid fa-envelope">Email</h3>
-                        <p>
-                          <a href="#">info@untitled.tld</a>
-                        </p>
-                      </section>
-                    </div>
-                    <div className="col-6 col-12-small">
-                      <section>
-                        <h3 className="icon solid fa-phone">Phone</h3>
-                        <p>(000) 555-0000</p>
-                      </section>
-                    </div>
-                  </div>
-                </section>
-              </div>
-            </div>
-            <div id="copyright">
-              <ul>
-                <li>&copy; Untitled.</li>
-                <li>
-                  Design: <a href="http://html5up.net">HTML5 UP</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
+
       </div>
 
       {/* <!-- Scripts --> */}
