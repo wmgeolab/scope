e2b0cf6f3fcf901a45a411dea600b91974766bd4
SawyerCzupka <sawyerczupka@gmail.com>
2023-02-18 16:50:23 -0500

    Complete Handler

--- NLP_Fall2022/PegasusHandler.py
+++ NLP_Fall2022/PegasusHandler.py
@@ -2,20 +2,79 @@ import torch
 import transformers
 import os
 import json
+import logging
 
 from ts.torch_handler.base_handler import BaseHandler
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
+logger = logging.getLogger(__name__)
+logger.info("Transformers version %s", transformers.__version__)
+
 
 class PegasusHandler(BaseHandler):
     def initialize(self, context):
-        return super().initialize(context)
+        properties = context.system_properties
+        self.manifest = context.manifest
+        model_dir = properties.get("model_dir")
 
-    def preprocess(self, data):
-        return super().preprocess(data)
+        # Use GPU if available
+        self.device = torch.device(
+            "cuda:" + str(properties.get("gpu_id"))
+            if torch.cuda.is_available() and properties.get("gpu_id") is not None
+            else "cpu"
+        )
 
-    def inference(self, data, *args, **kwargs):
-        return super().inference(data, *args, **kwargs)
+        logger.info(f"Using device {self.device}")
+
+        # Load model
+        model_file = self.manifest["model"]["modelFile"]
+        model_path = os.path.join(model_dir, model_file)
+
+        if os.path.isfile(model_path):
+            self.model = PegasusForConditionalGeneration.from_pretrained(model_dir)
+            self.model.to(self.device)
+            self.model.eval()
+            logger.info(f"Successfully loaded model from {model_file}")
+        else:
+            raise RuntimeError(f"Model file not found. Path: {model_path}")
+
+        # Load tokenizer
+        self.tokenizer = PegasusTokenizer.from_pretrained(model_dir)
+        if self.tokenizer is not None:
+            logger.info(f"Successfully loaded tokenizer")
+        else:
+            raise RuntimeError("Missing tokenizer")
+
+        self.initialied = True
+
+    def preprocess(self, requests):
+        # Unpack Data
+        data = requests[0].get("body")
+        if data is None:
+            data = requests[0].get("data")
+
+        texts = data.get("input")
+        logger.info(f"Received {len(texts)} texts. Begin tokenizing")
+
+        # Tokenize Texts
+        tokenized_data = self.tokenizer(
+            texts,
+            truncation=True,
+            padding="longest",
+            return_tensors="pt",
+        )
+
+        logger.info("Tokenization process completed")
+
+        return tokenized_data
+
+    def inference(self, inputs):
+        translated = self.model.generate(**inputs)
+        output = self.tokenizer.batch_decode(translated, skip_special_tokens=True)
+
+        logger.info(f"Summary successfully generated")
+
+        return output
 
     def postprocess(self, data):
-        return super().postprocess(data)
+        pass
