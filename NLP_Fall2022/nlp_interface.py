c1017c36c79d4ed20f84c39d9d6a9559227b9d68
SawyerCzupka <sawyerczupka@gmail.com>
2023-03-21 15:37:12 -0400

    Simplified Scripts

--- NLP_Fall2022/nlp_interface.py
+++ NLP_Fall2022/nlp_interface.py
@@ -3,18 +3,15 @@
 from newspaper import Article, Config
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import torch
+from pegasus_model import PegasusModel
 
 
 class SummaryFactory:
     """Class to deal with initializing the PEGASUS model and querying it with urls"""
 
     def __init__(self):
-        self.TORCH_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Model being loaded on device: {self.TORCH_DEVICE}")
-
-        self.model_initialized = False
-        self.initialize_pegasus_model()
-        print("Model Initialized")
+        self.model = PegasusModel()
+        print(f"Model being loaded on device: {self.model.TORCH_DEVICE}")
 
         self.initialize_newspaper_config()
 
@@ -25,38 +22,12 @@ class SummaryFactory:
         ## prevents timing out ...
         config.request_timeout = 30
 
-    def initialize_pegasus_model(self):
-        """Initializes the pegasus model and its tokenizer with the pretrained weights from hugging face"""
-
-        model_name = "google/pegasus-multi_news"
-
-        self.model = PegasusForConditionalGeneration.from_pretrained(model_name).to(
-            self.TORCH_DEVICE
-        )
-
-        self.tokenizer = PegasusTokenizer.from_pretrained(model_name)
-
-        self.model_initialized = True
-
     def summarize_url(self, url: str) -> str:
         article = Article(url)
         article.download()
         article.parse()
 
-        return self.summarize_text_pegasus(article.text)
-
-    def summarize_text_pegasus(self, text: str) -> str:
-        batch = self.tokenizer(
-            text,
-            truncation=True,
-            padding="longest",
-            return_tensors="pt",
-        ).to(self.TORCH_DEVICE)
-
-        translated = self.model.generate(**batch)
-        tgt_text = self.tokenizer.batch_decode(translated, skip_special_tokens=True)
-
-        return tgt_text
+        return self.model(article.text)
 
 
 if __name__ == "__main__":
