06324c3faf1a8c5a9c94eb2759f164753b7c5e65
Sawyer Czupka <sawyerczupka@gmail.com>
2023-04-03 16:55:31 -0400

    tested pipeline with text2text generation (failure)

--- NLP_Spring2023/luke_qa_model.py
+++ NLP_Spring2023/luke_qa_model.py
@@ -4,13 +4,21 @@ from transformers import LukeForQuestionAnswering, AutoTokenizer
 from transformers import pipeline
 
 
-class LukeModel(nn.module):
+class LukeModel(nn.Module):
     def __init__(self, model_name="studio-ousia/luke-base"):
-        self.luke = LukeForQuestionAnswering.from_pretrained(model_name)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        #self.luke = LukeForQuestionAnswering.from_pretrained(model_name)
+        #self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.LukeQA = pipeline(model=model_name, tokenizer=model_name, task='text2text-generation', framework='pt')
         
     def forward(self, text, question):
-        inputs = self.tokenizer(question, text, return_tensors='pt')
-        outputs = self.luke(**inputs)
-        
-        
+        # inputs = self.tokenizer(question, text, return_tensors='pt')
+        # outputs = self.luke(**inputs)
+        pass
+    
+    def test_pipeline(self):
+        return self.LukeQA("question: Where do I live? context: My name is Wolfgang and I live in Berlin")
+
+if __name__ == "__main__":
+    model = LukeModel()
+    
+    print(f"Output: {model.test_pipeline()}")
