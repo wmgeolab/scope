65f22573992bc42918133901c1786c85db55e330
Sawyer Czupka <sawyerczupka@gmail.com>
2023-04-03 20:10:24 -0400

    Added more debugging statements

--- NLP_Spring2023/models/luke_qa_model.py
+++ NLP_Spring2023/models/luke_qa_model.py
@@ -5,20 +5,46 @@ from transformers import pipeline
 
 
 class LukeModel(nn.Module):
-    def __init__(self, model_name="studio-ousia/luke-base"):
-        #self.luke = LukeForQuestionAnswering.from_pretrained(model_name)
-        #self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.LukeQA = pipeline(model=model_name, tokenizer=model_name, task='text2text-generation', framework='pt')
-        
+    def __init__(self, model_name="studio-ousia/luke-base", *args, **kwargs):
+        super(LukeModel, self).__init__(*args, **kwargs)
+        self.luke = LukeForQuestionAnswering.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        # self.LukeQA = pipeline(
+        #     model=model_name,
+        #     tokenizer=model_name,
+        #     task="text2text-generation",
+        #     framework="pt",
+        # )
+
     def forward(self, text, question):
-        # inputs = self.tokenizer(question, text, return_tensors='pt')
-        # outputs = self.luke(**inputs)
-        pass
-    
+        inputs = self.tokenizer(question, text, return_tensors="pt")
+        outputs = self.luke(**inputs)
+
+        answer_start_index = outputs.start_logits.argmax()
+        answer_end_index = outputs.end_logits.argmax()
+
+        predict_answer_tokens = inputs.input_ids[
+            0, answer_start_index : answer_end_index + 1
+        ]
+
+        print(f"Outputs Ids: {outputs}")
+        print(f"Predict_answer_tokens: {predict_answer_tokens}")
+        print(
+            f"Decoded prediction: {self.tokenizer.decode(predict_answer_tokens, skip_special_tokens=True)}"
+        )
+
     def test_pipeline(self):
-        return self.LukeQA("question: Where do I live? context: My name is Wolfgang and I live in Berlin")
+        return self.LukeQA(
+            "question: Where do I live? context: My name is Wolfgang and I live in Berlin"
+        )
+
 
 if __name__ == "__main__":
     model = LukeModel()
-    
-    print(f"Output: {model.test_pipeline()}")
+
+    question, text = (
+        "Who was Jim Henson?",
+        "Jim Henson was a nice puppet that liked to go on runs in the evening",
+    )
+
+    print(f"Output: {model.forward(text, question)}")
