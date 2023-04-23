3dc35f52ca078383c48a455d5f3178b3f2712d1d
Matthew Esposito <matt@matthew.science>
2023-04-23 14:22:38 -0400

    Add WorkspaceMembers, WorkspaceEntries model

--- backend/scopeBackend/models.py
+++ backend/scopeBackend/models.py
@@ -70,11 +70,21 @@ class Workspace(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120)
     owner = models.CharField(max_length=120)
-    user = models.ManyToManyField(User)
     tags = models.CharField(max_length=300)
-    sources = models.ForeignKey(Result, on_delete=models.PROTECT)
     password = models.CharField(max_length=120)
     status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+
+class WorkspaceMembers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+class WorkspaceEntries(models.Model):
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
