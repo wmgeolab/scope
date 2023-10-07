df84078cf76e23e74d015156c7a1f44d2e15d308
turtlu <109238722+turtlu@users.noreply.github.com>
2023-10-07 15:10:33 -0400

    Fixed radio button bug and added some post-loading functionality

--- frontend/src/components/QuestionForm.jsx
+++ frontend/src/components/QuestionForm.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
-import { Form, FormGroup, FormCheck } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import Spinner from 'react-bootstrap/Spinner';
 
 const QuestionForm = ({source}) => {
+
+const [disabled, setDisabled] = useState(false);
+const [AItext, setAItext] = useState("AI");
+const [text, setText] = useState("");
+
+const loading = () => {
+    setDisabled(true);
+    setTimeout(finished, 2000);
+}
+
+const finished = () => {
+    setDisabled(false);
+    setAItext("Regenerate");
+    setText("<AI generated text>");
+}
 
 return (
     
@@ -16,13 +32,24 @@ return (
             </h3>
             
             <div style={{float:"right"}}>
-                <input type="radio" class="btn-check"  name={`options-outlined-${source.id}`} id={`success-outlined-${source.id}`}   autocomplete="off" checked/>
-                <label class="btn btn-outline-primary" for={`success-outlined-${source.id}`}>AI</label>
-                <input type="radio" class="btn-check"name={`options-outlined-${source.id}`} id={`danger-outlined-${source.id}`}  autocomplete="off"/>
+                <input type="radio" class="btn-check"name={`options-outlined-${source.id}`} id={`danger-outlined-${source.id}`}  autocomplete="off" defaultChecked onClick={() => setDisabled(false)} disabled={disabled}/>
                 <label class="btn btn-outline-primary" for={`danger-outlined-${source.id}`}> Manual</label>
+                <input type="radio" class="btn-check"  name={`options-outlined-${source.id}`} id={`success-outlined-${source.id}`}   autocomplete="off"  onClick={() => loading()}/>
+                <label class="btn btn-outline-primary" for={`success-outlined-${source.id}`}>{AItext} {'   '}
+                    <Spinner
+                        as="span"
+                        animation="border"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                        hidden={!disabled}
+                    />
+                </label>
             </div>
 
-            <Form.Control as="textarea" />
+            
+
+            <Form.Control as="textarea" disabled={disabled} value={text} onChange={(e) => setText(e.target.value)}/>
             </Form.Group>
 
         </Form>
