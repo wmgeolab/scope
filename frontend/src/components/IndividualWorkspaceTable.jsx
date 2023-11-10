f531a6da4c06bfe36ff4318863239d0aeb76f8ef
Taylor Liegel <tliegel@wm.edu>
2023-11-10 14:11:26 -0500

    fix

--- frontend/src/components/IndividualWorkspaceTable.jsx
+++ frontend/src/components/IndividualWorkspaceTable.jsx
@@ -1,77 +1,52 @@
 import React, { useState } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Container } from "@mui/material";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-export default function IndividualWorkspaceTable(props) {
-  const { data } = props;
 
-  console.log(data);
-  const columns = [
-    { field: "id", headerName: "ID", width: 90 },
+export default function IndividualWorkspaceTable(props){
+    const { data } = props;
 
-    {
-      field: "wsName",
-      headerName: "Name",
-      width: 150,
-    },
+    console.log(data);
+    const columns = [
+        { field: "id", headerName: "ID", width: 90 },
+      
+        {
+          field: "wsName",
+          headerName: "Name",
+          width: 150,
+        },
+      
+        {
+          field: "wsURL",
+          headerName: "URL",
+          width: 150,
+          flex: 1,
+          renderCell: (cellValue) => {
+            return <a href={cellValue.formattedValue}>{cellValue.formattedValue}</a>;
+          },
+        },
+      ];
 
-    {
-      field: "wsURL",
-      headerName: "URL",
-      width: 150,
-      flex: 1,
-      renderCell: (cellValue) => {
-        return (
-          <a href={cellValue.formattedValue}>{cellValue.formattedValue}</a>
-        );
-      },
-    },
-  ];
+    return(
+        // Put back in filtmodel
+        <Container disableGutters sx={{ height: 400, marginTop: "2%"}}>
+            <DataGrid
+            rows={data}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            disableColumnFilter
+            disableColumnMenu
+            disableDensitySelector
+            disableColumnSelector
+            disableSelectionOnClick
+            experimentalFeatures={{ newEditingApi: true }}
+            components={{ Toolbar: GridToolbar }}
+      />
+        </Container>
+    );
 
-  return (
-    // Put back in filtmodel
-    <Container
-      disableGutters
-      sx={{
-        height: 400,
-        marginTop: "2%",
-        justifyContent: "left",
-        alignSelf: "left",
-        alignContent: "left",
-      }}
-    >
-      {data.map((entry) => (
-        <Row className="text-start mt-3">
-          <Col sm={10}>
-            <h6>Source ID: {entry.id}</h6>
-            <Row>
-              <h4>
-                <b>Article Name : {entry.wsName}</b>
-              </h4>
-            </Row>
-            <h6>
-              <a href={entry.wsURL}> {entry.wsURL}</a>
-            </h6>
 
-            <Row>
-              <Col sm={5}>
-                <Button variant="outline-danger">Delete</Button>
-                <Button variant="outline-primary">Edit</Button>
-              </Col>
-            </Row>
-          </Col>
-          {/* <Col>
-              {entry.id}
-              </Col>
-              <Col>
-              {entry.wsName}
-              </Col>
-              <Col>
-              {entry.wsURL}
-              </Col> */}
-        </Row>
-      ))}
-    </Container>
-  );
 }
