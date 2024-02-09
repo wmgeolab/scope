9459ab4444f519537f5b1e296526edb1503f212a
Matthew Esposito <matt@matthew.science>
2024-02-09 15:04:21 -0500

    Fix typo in filename

---
+++ frontend/src/components/IndividualWorkspacePage.jsx
@@ -0,0 +1,118 @@
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Row, Button, Col, Form, InputGroup } from "react-bootstrap";
+import { Container } from "@mui/material";
+import { Search } from "react-bootstrap-icons";
+import UnauthorizedView from "./UnauthorizedView";
+import IndividualWorkspaceTable from "./IndividualWorkspaceTable";
+
+export default function IndividualWorkspacePage(props) {
+  const { loggedIn } = props;
+  const { workspace_name, workspace_id } = useParams();
+  const [showModal, setShowModal] = useState(false);
+
+  const [workspaceSources, setWorkspaceSources] = useState([]);
+
+  var textInput = React.createRef();
+  const [filt, setFilt] = useState([]);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const onSubmitSearch = (event) => {
+    event.preventDefault();
+    console.log(
+      "The input string being passed here is: ",
+      textInput.current.value
+    );
+
+    setFilt([
+      {
+        columnField: "wsName",
+        operatorValue: "contains",
+        value: textInput.current.value,
+      },
+    ]);
+  };
+
+  async function obtainSources() {
+    const response = await fetch("http://127.0.0.1:8000/api/entries/?workspace=" + workspace_id, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + localStorage.getItem("user"),
+      },
+    });
+
+    const response_text = await response.json();
+    const formattedResponse = response_text.results.map(result => {
+      return {
+        id: result.source.id,
+        wsName: result.source.text,
+        wsURL: result.source.url,
+      }
+    });
+
+    if (formattedResponse)
+      setWorkspaceSources(formattedResponse);
+  }
+
+  useEffect(() => {
+    obtainSources();
+  }, []);
+
+  if (loggedIn === false) {
+    return <UnauthorizedView />;
+  } else {
+    return (
+      <Container>
+        <Row className="mt-5">
+          <Col sm={1}>
+            <Button
+              variant="link"
+              href="/workspaces"
+              style={{
+                color: "rgb(48, 46, 46)",
+              }}
+            >
+              🡸 Return home
+            </Button>
+          </Col>
+          <Col sm={4} className="text-start">
+            <h1>
+              <b>{workspace_name}</b>
+            </h1>
+          </Col>
+          <Col sm={3} />
+          <Col sm={4} className="float-end mt-2">
+            <Form onSubmit={onSubmitSearch}>
+              <InputGroup>
+                <InputGroup.Text>
+                  <Search></Search>
+                </InputGroup.Text>
+                <Form.Control
+                  placeholder="Search by Article Name"
+                  ref={textInput}
+                  onChange={null}
+                  type="text"
+                />
+              </InputGroup>
+            </Form>
+          </Col>
+        </Row>
+        <IndividualWorkspaceTable data={workspaceSources} filt={filt}/>
+        <Row className="mt-5">
+          <Col sm={5}/>
+          <Col sm={2}>
+            <Button onClick={handleShowModal}>Share Workspace</Button>
+          </Col>
+          <Col sm={5}/>
+        </Row>
+      </Container>
+    );
+  }
+}
