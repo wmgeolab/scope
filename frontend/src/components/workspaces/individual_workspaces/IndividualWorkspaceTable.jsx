3c467936c261230dd9f43501484c7f3c9ae46b09
Taylor Liegel <tliegel@wm.edu>
2024-03-01 10:40:10 -0500

    Adding toast for generation

--- frontend/src/components/workspaces/individual_workspaces/IndividualWorkspaceTable.jsx
+++ frontend/src/components/workspaces/individual_workspaces/IndividualWorkspaceTable.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Container, Button } from "@mui/material";
+import Snackbar from '@mui/material/Snackbar';
 
 export default function IndividualWorkspaceTable({ data, filt }) {
   const { workspace_name, workspace_id } = useParams();
+  const [toastGenerate, setToastGenerate] = React.useState(false);
+  const [message, setMessage] = React.useState("Triggering article generation!");
+
   const columns = [
     { field: "id", 
     headerName: "ID"},
@@ -18,7 +22,7 @@ export default function IndividualWorkspaceTable({ data, filt }) {
     {
       field: "wsURL",
       headerName: "URL",
-      flex: 4,
+      flex: 3,
       renderCell: (cellValue) => {
         return (
           <a href={cellValue.formattedValue}>{cellValue.formattedValue}</a>
@@ -34,13 +38,37 @@ export default function IndividualWorkspaceTable({ data, filt }) {
           // <Button class="btn btn-primary btn-sm" href="/questions">
           //   Generate
           // </Button>
-            <Button variant="contained" style={{'textTransform':'capitalize', 'width':'60%', 'alignContent':'right'}} type="submit" href={"/questions/" + workspace_name} >
+            <Button variant="contained" style={{'textTransform':'capitalize', 'width':'60%', 'alignContent':'right'}} onClick={() => handleGenerateBtnClick(params.id)} >
               Generate
             </Button>
         );
       },
     },
+    {
+      field: "wsView",
+      headerName: "View AI Summary",
+      flex: 1,
+      renderCell: (params) => {
+        return (
+            <Button variant="contained" style={{'textTransform':'capitalize', 'width':'60%', 'alignContent':'right'}} type="submit" href={"/questions/" + workspace_name}>
+             View
+            </Button>
+        );
+      },
+    },
   ];
+
+  console.log(data);
+
+  const handleGenerateBtnClick = (article_id) => {
+    let article_string = "Requesting AI generation for article with ID: " + article_id;
+    setMessage(article_string);
+    setToastGenerate(true);
+  }
+
+  const handleGenerateBtnClose = () => {
+    setToastGenerate(false);
+  }
 
   return (
     <Container disableGutters sx={{ height: 400, marginTop: "2%" }}>
@@ -61,6 +89,12 @@ export default function IndividualWorkspaceTable({ data, filt }) {
           items: filt,
         }}
       />
+      <Snackbar
+        open={toastGenerate}
+        onClose={handleGenerateBtnClose}
+        autoHideDuration={3000}
+        message={message}
+      ></Snackbar>
     </Container>
   );
 }
