70317da1ef940506b9e9a13894040c5329487783
lgwallis <lgwallis@wm.edu>
2024-12-02 21:52:00 -0500

    added separate files for hurl testing

---
+++ backend/scopeBackend/Hurl Endpoint Testing/All Tags from Specified Workspace.py
@@ -0,0 +1,55 @@
+#Getting all tags from a specified workspace 
+POST http://127.0.0.1:8000/api/workspaces/
+Authorization: Token fadc41688e2228acb6c2cd435362f4eda6f4130d
+Content-Type: application/json
+
+{
+    "name": "test888",
+    "password": "test888"
+}
+HTTP 201
+[Captures]
+id: jsonpath "$.id"
+
+POST http://127.0.0.1:8000/api/tags/
+Authorization: Token fadc41688e2228acb6c2cd435362f4eda6f4130d
+Content-Type: application/json
+
+{
+    "workspace": {{id}}, 
+    "tag": "tag1",
+    "tag": "tag2"
+}
+HTTP 201
+
+
+GET http://127.0.0.1:8000/api/tags/
+Authorization: Token fadc41688e2228acb6c2cd435362f4eda6f4130d
+Content-Type: application/json
+
+{
+    "workspace": {{id}}
+}
+
+HTTP 200
+
+DELETE http://127.0.0.1:8000/api/tags/
+Authorization: Token fadc41688e2228acb6c2cd435362f4eda6f4130d
+Content-Type: application/json
+
+{
+    "workspace": {{id}}, 
+    "tag": "tag1",
+    "tag": "tag2"
+}
+HTTP 200
+
+DELETE http://127.0.0.1:8000/api/workspaces/ 
+Authorization: Token fadc41688e2228acb6c2cd435362f4eda6f4130d
+Content-Type: application/json
+
+{
+    "name": "test888",
+    "password": "test888"
+}
+HTTP 200
