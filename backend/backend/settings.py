6cbdbf7c61eaa2b938f748c4dabce0a287b56c59
Matthew Esposito <matt@matthew.science>
2025-01-29 16:09:28 -0500

    fix(backend): accept backend hostnames

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'dqno9jj#gx=rw5-c!(6dqv_$f#qvg_qmrza%1xf91ip167^tjh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend', 'scope-backend']
 
 
 # Application definition
