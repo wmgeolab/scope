342c3db19f3ac8b6b6edaf42c86e6cf8996219ef
Matthew Esposito <matt@matthew.science>
2025-03-31 10:24:59 -0400

    chore: add logging

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -8,6 +8,7 @@ export class API {
    * @returns {string} The complete API URL
    */
   static url(path = '') {
+    console.log(process.env.REACT_APP_API_URL)
     const baseURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
     return path ? `${baseURL}${path}` : baseURL;
   }
