a568113009e858e1fc5fb34beef488d3268f3980
Matthew Esposito <matt@matthew.science>
2025-03-31 11:15:02 -0400

    fix: cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,7 +91,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # delete later
     "http://127.0.0.1:8000",
-    "https://backend.scnw.cc"
+    "https://frontend.scnw.cc"
 ]
 
 ROOT_URLCONF = 'backend.urls'
